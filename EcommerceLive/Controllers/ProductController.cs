402b2042b5840173f346283be30108be1918a003
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-18 12:06:54 +0100

    Aggiungere i file di progetto.

---
+++ EcommerceLive/Controllers/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using EcommerceLive.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceLive.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Pc",
+                Description = "A powerful pc",
+                Category = "Electronics",
+                Price = 1000,
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "TV",
+                Description = "A powerful Tv",
+                Category = "Electronics",
+                Price = 700,
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Book",
+                Description = "A nice book",
+                Category = "Literature",
+                Price = 20,
+            },
+        };
+
+        public IActionResult Index()
+        {
+            var productsList = new ProductsViewModel() { Products = products };
+            return View(productsList);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // attributo
+        [HttpPost]
+        public IActionResult Add(AddProductModel addProductModel)
+        {
+            var newProduct = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = addProductModel.Name,
+                Description = addProductModel.Description,
+                Category = addProductModel.Category,
+                Price = addProductModel.Price,
+            };
+
+            products.Add(newProduct);
+
+            // Il controller non viene passato perché è già il presente
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EcommerceLive/Models/AddProductModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EcommerceLive.Models
+{
+    public class AddProductModel
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Category { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
