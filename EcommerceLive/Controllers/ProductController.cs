5917e1d3fedb22365aa53b6ebd4066dc113b26e7
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-19 10:49:28 +0100

    modify product

--- EcommerceLive/Controllers/ProductController.cs
+++ EcommerceLive/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ namespace EcommerceLive.Controllers
         {
             new Product()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("6628bc9c-4168-44fb-8f88-b8b75ccc1970"),
                 Name = "Pc",
                 Description = "A powerful pc",
                 Category = "Electronics",
@@ -17,7 +17,7 @@ namespace EcommerceLive.Controllers
             },
             new Product()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("993b12fd-9965-4600-9e6a-c78c578f8544"),
                 Name = "TV",
                 Description = "A powerful Tv",
                 Category = "Electronics",
@@ -25,7 +25,7 @@ namespace EcommerceLive.Controllers
             },
             new Product()
             {
-                Id = Guid.NewGuid(),
+                Id = Guid.Parse("d73c6b22-edd2-4c3b-836e-604f86782cb9"),
                 Name = "Book",
                 Description = "A nice book",
                 Category = "Literature",
@@ -39,6 +39,7 @@ namespace EcommerceLive.Controllers
             return View(productsList);
         }
 
+        // Action metod per la navigazione verso la vista identificata dal file Add.cshtml
         public IActionResult Add()
         {
             return View();
@@ -48,6 +49,11 @@ namespace EcommerceLive.Controllers
         [HttpPost]
         public IActionResult Add(AddProductModel addProductModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Add");
+            }
+
             var newProduct = new Product()
             {
                 Id = Guid.NewGuid(),
@@ -60,6 +66,48 @@ namespace EcommerceLive.Controllers
             products.Add(newProduct);
 
             // Il controller non viene passato perché è già il presente
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("product/edit/{id:guid}")]
+        public IActionResult Edit(Guid id)
+        {
+            // cerco l'oggetto tramite id nella lista statica dei prodotti
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+
+            if (existingProduct == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var editProduct = new EditProduct()
+            {
+                Id = existingProduct.Id,
+                Name = existingProduct.Name,
+                Description = existingProduct.Description,
+                Category = existingProduct.Category,
+                Price = existingProduct.Price,
+            };
+
+            return View(editProduct);
+        }
+
+        [HttpPost("product/edit/save/{id:guid}")]
+        public IActionResult SaveEdit(Guid id, EditProduct editProduct)
+        {
+            // cerco l'oggetto tramite id nella lista statica dei prodotti
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+
+            if (existingProduct == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            existingProduct.Name = editProduct.Name;
+            existingProduct.Description = editProduct.Description;
+            existingProduct.Category = editProduct.Category;
+            existingProduct.Price = editProduct.Price;
+
             return RedirectToAction("Index");
         }
     }
