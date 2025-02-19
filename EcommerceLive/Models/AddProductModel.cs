1cb879e1f91d5d5cadf9418a9d132479fe9cf67a
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-19 11:20:04 +0100

    more validation attributes to addproduct model

--- EcommerceLive/Models/AddProductModel.cs
+++ EcommerceLive/Models/AddProductModel.cs
@@ -6,10 +6,12 @@ namespace EcommerceLive.Models
     {
         [Display(Name = "Name")]
         [Required(ErrorMessage = "Il nome è obligatorio")]
+        [StringLength(20, ErrorMessage = "max 20 chars", MinimumLength = 2)]
         public string? Name { get; set; }
 
         [Display(Name = "Description")]
         [Required(ErrorMessage = "La descrizione è obligatoria")]
+        [StringLength(2000, ErrorMessage = "max 2000 chars", MinimumLength = 10)]
         public string? Description { get; set; }
 
         [Display(Name = "Category")]
@@ -18,6 +20,7 @@ namespace EcommerceLive.Models
 
         [Display(Name = "Price")]
         [Required(ErrorMessage = "Il prezzo è obligatorio")]
+        [Range(0.01, 1000000, ErrorMessage = "Range from 0,01 and 1000000")]
         public decimal? Price { get; set; }
     }
 }
